a4d952e986a31199e6268f4d921358e92adeca3b
Gregory Fischer <gwhoisj@gmail.com>
2022-01-15 18:08:02 -0500

    Feat: create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    if params[:password] == params[:password_confirmation]
+      user = User.create!(user_params)
+      user.set_api_key
+      render json: UserSerializer.new(user), status: 201
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
   end
 end
