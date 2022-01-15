475785bf9f93d32956abdbb7647807ed731134d1
Gregory Fischer <gwhoisj@gmail.com>
2022-01-15 18:07:51 -0500

    Refactor: different possible solution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  def request_info
-    JSON.parse(request.body.read, symbolize_names: true)
-  end
+  # def request_info
+  #   JSON.parse(request.body.read, symbolize_names: true)
+  # end
 end
