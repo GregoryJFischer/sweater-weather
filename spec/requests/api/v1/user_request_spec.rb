6da814e0d4c11ec6d0fed8abda3530f971437ef4
Gregory Fischer <gwhoisj@gmail.com>
2022-01-15 21:09:56 -0500

    Test: add sad path

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -26,5 +26,35 @@ describe 'user requests' do
       expect(attributes[:api_key]).to_not be_nil
       expect(attributes[:api_key]).to be_a String
     end
+
+    it 'returns an error if no information is sent' do
+      post '/api/v1/users', params: {}, as: :json, headers: @headers
+
+      expect(response.status).to eq 422
+      expect(response.body).to match(/Email can't be blank, Password can't be blank/)
+    end
+
+    it 'returns an error with partial information' do
+      post '/api/v1/users', params: {email: 'test@example.com'}, as: :json, headers: @headers
+
+      expect(response.status).to eq 422
+      expect(response.body).to match(/Password can't be blank/)
+    end
+
+    it 'returns an error if password and confirmation do not match' do
+      @json_payload[:password] = 'not_password'
+      post '/api/v1/users', params: @json_payload, as: :json, headers: @headers
+
+      expect(response.status).to eq 422
+      expect(response.body).to match(/Password and confirmation do not match/)
+    end
+
+    it 'returns an error if email is reused' do
+      post '/api/v1/users', params: @json_payload, as: :json, headers: @headers
+      post '/api/v1/users', params: @json_payload, as: :json, headers: @headers
+
+      expect(response.status).to eq 422
+      expect(response.body).to match(/Validation failed: Email has already been taken/)
+    end
   end
 end
