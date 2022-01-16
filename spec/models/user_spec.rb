ae47454a060a6fc03769df5b856df75ea75567f2
Gregory Fischer <gwhoisj@gmail.com>
2022-01-15 21:09:34 -0500

    Test: update user test to reflect db changes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,9 @@ describe User, type: :model do
     it {should validate_presence_of :email}
     it {should validate_uniqueness_of :email}
     it {should validate_presence_of :password}
-    it {should validate_uniqueness_of :api_key}
+  end
+
+  describe 'relationships' do
+    it {should have_many :keys}
   end
 end
