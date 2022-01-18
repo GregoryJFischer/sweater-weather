795d0ef23d31de1a80cc611c994e41a46e0ff797
Gregory Fischer <gwhoisj@gmail.com>
2022-01-18 15:39:57 -0500

    Feat: correct which weather is showing

--- spec/requests/road_trip_request_spec.rb
+++ spec/requests/road_trip_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'road trip requests' do
+describe 'road trip requests', :vcr do
   describe 'POST /api/v1/road_trip' do
     before :each do
       user = create(:user)
@@ -18,7 +18,20 @@ describe 'road trip requests' do
       it 'gives the weather at the destination' do
         post '/api/v1/road_trip', params: @json_payload, as: :json, headers: @headers
 
-        
+        expect(response.status).to eq 200
+
+        info = JSON.parse(response.body, symbolize_names: true)
+        data = info[:data]
+        attributes = data[:attributes]
+
+        expect(data[:id]).to be_nil
+        expect(data[:type]).to eq 'roadtrip'
+        expect(attributes[:start_city]).to eq @json_payload[:origin]
+        expect(attributes[:end_city]).to eq @json_payload[:destination]
+        expect(attributes[:travel_time]).to be_a String
+        expect(attributes[:weather_at_eta]).to be_a Hash
+        expect(attributes[:weather_at_eta][:temperature]).to be_a Float
+        expect(attributes[:weather_at_eta][:conditions]).to be_a String
       end
     end
   end
